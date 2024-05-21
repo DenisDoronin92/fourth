8ab26e3c8a6ec1396c857063acaf5fa55b229d1d
Денис Доронин <denis_doronin@rambler.ru>
2024-05-21 16:15:14 +0400

    Мой 4й коммит

---
+++ main.js
@@ -0,0 +1,49 @@
+// console.log(6);
+// console.log('Добро '+'пожаловать '+'на курс ' + 'Денис');
+// // alert('15.05.2024')
+// document.getElementById('one').innerHTML = 'Hello world2'
+// console.log(typeof 1);
+// console.log(typeof 'cat');
+// console.log(typeof 100n);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// let btn = document.querySelector('.btn')
+// let inputIn = document.querySelector('.inp')
+// let div = document.querySelector('.ourr')
+
+// btn.onclick = function(){
+// let e = inputIn.value
+
+// if (e > 100) {
+//     console.log(e);
+// } else {
+//     div.innerHTML += `<li>${e}</li>`
+// }
+
+// }
+
+// 1 задание 4 занятия
+
+let a = prompt ('Значиние а');
+let b = prompt ('Значиние b');
+
+if (Number(a) + Number(b) <= 15) {
+    console.log('НЕТ');
+} else {
+    console.log('ДА')
+}
+
+
+// 2 задание 4 занятия
+
+let d = prompt ('Значение 1й переменной')
+let e = prompt ('Значение 2й переменной')
+if (d === e) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
